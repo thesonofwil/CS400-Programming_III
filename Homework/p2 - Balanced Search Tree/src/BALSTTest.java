01cff74b5c9fd0ca583bc0bbe433d68b1fd08afa
Wilson Tjoeng <thesonofwil@gmail.com>
2021-10-09 21:45:49 -0500

    Added several more tests

--- Homework/p2 - Balanced Search Tree/src/BALSTTest.java
+++ Homework/p2 - Balanced Search Tree/src/BALSTTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -199,15 +203,83 @@ public class BALSTTest {
     // If delete is not implemented, does calling it throw an UnsupportedOperationException
     
     @Test
-    void testBST_005_insert_simple_sorted_delete_root() {
+    void testBST_005_IllegalNullKeyException_thrown_insert() {
+    	try {
+    		bst.insert(null, "5");
+    	} catch(IllegalNullKeyException e) {
+    		assert(bst.numKeys() == 0);
+    		assert(bst.getHeight() == 0);
+    	} catch(Exception e) {
+    		fail("Illegal null key exception not thrown. "  +e.getMessage());
+    	}
+    }
+    
+    @Test
+    void testBST_006_DuplicateKeyException_thrown_insert() {
+    	try {
+    		bst.insert(10, "10");
+    		bst.insert(10, "20");
+    	} catch(DuplicateKeyException e) {
+    		assert(bst.numKeys() == 1);
+    		assert(bst.getHeight() == 1);
+    	} catch(Exception e) {
+    		fail("Duplicate key exception not thrown. " +e.getMessage());
+    	}
+    }
+    
+    @Test
+    void testBST_007_IllegalNullKeyException_thrown_remove() {
+    	try {
+    		bst.remove(null);
+    	} catch(IllegalNullKeyException e) {
+    		assert(bst.numKeys() == 0);
+    		assert(bst.getHeight() == 0);
+    	} catch(Exception e) {
+    		fail("Illegal null key exception not thrown. "  +e.getMessage());
+    	}
+    }
+    
+    @Test
+    void testBST_008_IllegalNullKeyException_thrown_get() {
+    	try {
+    		bst.get(null);
+    	} catch(IllegalNullKeyException e) {
+    	} catch(Exception e) {
+    		fail("Illegal null key exception not thrown. "  +e.getMessage());
+    	}
+    }
+    
+    @Test
+    void testBST_009_KeyNotFoundException_thrown_get() {
+    	try {
+    		bst.get(10);
+    	} catch(KeyNotFoundException e) {
+    	} catch(Exception e) {
+    		fail("Illegal null key exception not thrown. "  +e.getMessage());
+    	}
+    }
+    
+    @Test
+    void testBST_010_IllegalNullKeyException_thrown_contains() {
+    	try {
+    		bst.contains(null);
+    	} catch(IllegalNullKeyException e) {
+    	} catch(Exception e) {
+    		fail("Illegal null key exception not thrown. "  +e.getMessage());
+    	}
+    }
+    	
+    @Test
+    void testBST_011_insert_simple_sorted_delete_root() {
     	try {
 			bst.insert(10, "10");
 			bst.insert(20, "20");
 		    bst.insert(30, "30");
-		    assert bst.numKeys() == 3;
-		    assert bst.remove(20);
-		    assert bst.getKeyAtRoot() == 10;
-		    assert bst.numKeys() == 2;
+		    assert(bst.numKeys() == 3);
+		    assert(bst.remove(20));
+		    assert(bst.getKeyAtRoot() == 10);
+		    assert(bst.numKeys() == 2);
+		    assert(bst.getHeight() == 2);
 		} catch (Exception e) {
 			e.printStackTrace();
             fail( "Unexpected exception: "+e.getMessage() );
@@ -215,15 +287,16 @@ public class BALSTTest {
     }
     
     @Test
-    void testBST_006_insert_simple_rotate_left_right() {
+    void testBST_012_insert_simple_rotate_left_right() {
     	try {
 			bst.insert(20, "20");
 			bst.insert(10, "10");
 		    bst.insert(15, "15");
-		    assert bst.numKeys() == 3;
-		    assert bst.getKeyAtRoot() == 15;
-		    assert bst.getKeyOfLeftChildOf(15) == 10;
-		    assert bst.getKeyOfRightChildOf(15) == 20; 
+		    assert(bst.numKeys() == 3);
+		    assert(bst.getHeight() == 2);
+		    assert(bst.getKeyAtRoot() == 15);
+		    assert(bst.getKeyOfLeftChildOf(15) == 10);
+		    assert(bst.getKeyOfRightChildOf(15) == 20); 
 		} catch (Exception e) {
 			e.printStackTrace();
             fail( "Unexpected exception: "+e.getMessage() );
@@ -232,15 +305,16 @@ public class BALSTTest {
     }
     
     @Test
-    void testBST_007_insert_simple_rotate_right_left() {
+    void testBST_013_insert_simple_rotate_right_left() {
     	try {
 			bst.insert(20, "20");
 			bst.insert(30, "30");
 		    bst.insert(25, "25");
-		    assert bst.numKeys() == 3;
-		    assert bst.getKeyAtRoot() == 25;
-		    assert bst.getKeyOfLeftChildOf(25) == 20;
-		    assert bst.getKeyOfRightChildOf(25) == 30; 
+		    assert(bst.numKeys() == 3);
+		    assert(bst.getHeight() == 2);
+		    assert(bst.getKeyAtRoot() == 25);
+		    assert(bst.getKeyOfLeftChildOf(25) == 20);
+		    assert(bst.getKeyOfRightChildOf(25) == 30); 
 		} catch (Exception e) {
 			e.printStackTrace();
             fail( "Unexpected exception: "+e.getMessage() );
@@ -248,11 +322,33 @@ public class BALSTTest {
     	bst.print();
     }
     
-    // Tests 8 - 10 model after RBT Insert Practice from lecture notes
+    @Test 
+    void testBST_014_insert_and_remove_change_height() {
+    	try {
+    		assert(bst.getHeight() == 0);
+			bst.insert(20, "20");
+		    assert(bst.getHeight() == 1);
+			bst.insert(30, "30");
+		    assert(bst.getHeight() == 2);
+		    bst.insert(25, "25");
+		    assert(bst.getHeight() == 2);
+		    assert(bst.remove(20));
+		    assert(bst.remove(30));
+		    assert(bst.getHeight() == 1);
+		    assert(bst.remove(25));
+		    assert(bst.getHeight() == 0);
+		} catch (Exception e) {
+			e.printStackTrace();
+            fail( "Unexpected exception: "+e.getMessage() );
+		}
+    	bst.print();
+    }
+    
+    // Tests 15 - 20 model after RBT Insert Practice from lecture notes
     
     // After recoloring, insert again which should not trigger an RPV
     @Test
-    void testBST_008_insert_recolor_insert_no_violation() {
+    void testBST_015_insert_recolor_insert_no_violation() {
     	try {
     		bst.insert(7, "7");
     		bst.insert(14, "14");
@@ -271,7 +367,7 @@ public class BALSTTest {
     }
     
     @Test
-    void testBST_009_RPV_after_Recolor() {
+    void testBST_016_RPV_after_Recolor() {
     	try {
     		bst.insert(7, "7");
     		bst.insert(14, "14");
@@ -284,6 +380,7 @@ public class BALSTTest {
     		assert(bst.getKeyOfRightChildOf(14) == 20);
     		assert(bst.getKeyOfLeftChildOf(20) == 18);
     		assert(bst.getKeyOfRightChildOf(20) == 23);
+    		assert(bst.getHeight() == 3);
     	} catch (Exception e) {
 			e.printStackTrace();
             fail( "Unexpected exception: "+e.getMessage() );
@@ -292,7 +389,7 @@ public class BALSTTest {
     }
     
     @Test
-    void testBST_010_insert_cascade_rotates() {
+    void testBST_017_insert_cascade_rotates() {
     	try {
     		bst.insert(7, "7");
     		bst.insert(14, "14");
@@ -325,7 +422,7 @@ public class BALSTTest {
     }
     
     @Test 
-    void testBST_011_remove_parent_with_two_children() {
+    void testBST_018_remove_parent_with_two_children() {
     	try {
     		bst.insert(7, "7");
     		bst.insert(14, "14");
@@ -338,41 +435,127 @@ public class BALSTTest {
     		bst.insert(25, "25"); 
     		bst.insert(27, "27"); 
     		bst.remove(25);
-    		assert (!bst.contains(25));
-    		assert (bst.getKeyOfRightChildOf(20) == 23);
-    		assert (bst.numKeys() == 9);
+    		assert(!bst.contains(25));
+    		assert(bst.getKeyOfRightChildOf(20) == 23);
+    		assert(bst.numKeys() == 9);
     	} catch (Exception e) {
     		e.printStackTrace();
     		fail( "Unexpected exception: "+e.getMessage() );
     	}
     }
     	
-    	@Test 
-    	void testBST_012_remove_parent_with_two_children_and_grandchildren() {
-    	    	try {
-    	    		bst.insert(7, "7");
-    	    		bst.insert(14, "14");
-    	    		bst.insert(18, "18");
-    	    		bst.insert(23, "23");
-    	    		bst.insert(1, "1");
-    	    		bst.insert(11, "11");
-    	    		bst.insert(20, "20"); 
-    	    		bst.insert(29, "29"); 
-    	    		bst.insert(25, "25"); 
-    	    		bst.insert(27, "27"); 
-    	    		bst.insert(21, "21");
-    	    		bst.insert(24, "24");
-    	    		bst.insert(30, "30");
-    	    		bst.remove(25); // Remove node with two children and four grandchildren
-    	    		assert (!bst.contains(25));
-    	    		assert (bst.getKeyOfRightChildOf(20) == 24);
-    	    		assert (bst.getKeyOfLeftChildOf(24) == 23);
-    	    		assert (bst.getKeyOfRightChildOf(23) == null);
-    	    		assert (bst.getKeyOfLeftChildOf(23) == 21);
-    	    		assert (bst.numKeys() == 12);
-    	    	} catch (Exception e) {
-    	    		e.printStackTrace();
-    	    		fail( "Unexpected exception: "+e.getMessage() );
-    	    	}	
-    	}
+    @Test 
+    void testBST_019_remove_parent_with_two_children_and_grandchildren() {
+    	try {
+    		bst.insert(7, "7");
+    		bst.insert(14, "14");
+    		bst.insert(18, "18");
+    		bst.insert(23, "23");
+    		bst.insert(1, "1");
+    		bst.insert(11, "11");
+    		bst.insert(20, "20"); 
+    		bst.insert(29, "29"); 
+    		bst.insert(25, "25"); 
+    		bst.insert(27, "27"); 
+    		bst.insert(21, "21");
+    		bst.insert(24, "24");
+    		bst.insert(30, "30");
+    		bst.remove(25); // Remove node with two children and four grandchildren
+    		assert(!bst.contains(25));
+    		assert(bst.getKeyOfRightChildOf(20) == 24);
+    		assert(bst.getKeyOfLeftChildOf(24) == 23);
+    		assert(bst.getKeyOfRightChildOf(23) == null);
+    		assert(bst.getKeyOfLeftChildOf(23) == 21);
+    		assert(bst.numKeys() == 12);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		fail( "Unexpected exception: "+e.getMessage() );
+    	}	
+    }
+    	
+	@Test
+	void testBST_020_remove_root_insert_again() {
+		try {
+    		bst.insert(7, "7");
+    		bst.insert(14, "14");
+    		bst.insert(18, "18");
+    		bst.insert(23, "23");
+    		bst.insert(1, "1");
+    		bst.insert(11, "11");
+    		bst.insert(20, "20"); 
+    		bst.insert(29, "29"); 
+    		bst.insert(25, "25"); 
+    		bst.insert(27, "27");
+    		assert(bst.getKeyAtRoot() == 20);
+    		assert(bst.numKeys() == 10);
+    		assert(bst.remove(20)); // 18 should take place
+    		assert!(bst.contains(20));
+    		assert(bst.numKeys() == 9);
+    		assert(bst.getKeyAtRoot() == 18);
+    		assert(bst.get(18).equals("18"));
+    		bst.insert(20, "20");
+    		assert(bst.numKeys() == 10);
+    		assert(bst.getKeyOfLeftChildOf(23) == 20); // No rotation should occur
+		} catch (Exception e) {
+    		e.printStackTrace();
+    		fail( "Unexpected exception: "+e.getMessage() );
+    	}	
+		bst.print();
 	}
+	
+	@Test
+	void testBST_021_remove_parent_insert_again() {
+		try {
+    		bst.insert(7, "7");
+    		bst.insert(14, "14");
+    		bst.insert(18, "18");
+    		bst.insert(23, "23");
+    		bst.insert(1, "1");
+    		bst.insert(11, "11");
+    		bst.insert(20, "20"); 
+    		bst.insert(29, "29"); 
+    		bst.insert(25, "25"); 
+    		bst.insert(27, "27");
+    		assert(bst.numKeys() == 10);
+    		assert(bst.remove(14)); // 11 should take place
+    		assert(!bst.contains(14));
+    		assert(bst.numKeys() == 9);
+    		assert(bst.getKeyOfLeftChildOf(20) == 11);
+    		assert(bst.get(11).equals("11"));
+    		assert(bst.getKeyOfLeftChildOf(11) == 7);
+    		assert(bst.getKeyOfRightChildOf(11) == 18);
+    		bst.insert(14, "14");
+    		assert (bst.numKeys() == 10);
+    		assert(bst.getKeyOfLeftChildOf(18) == 14);
+		} catch (Exception e) {
+    		e.printStackTrace();
+    		fail( "Unexpected exception: "+e.getMessage() );
+    	}	
+		bst.print();
+	}
+	
+	@Test
+	void testBST_022_inorder_traversal() {
+		List<Integer> expected = Arrays.asList(1, 7, 11, 14, 18, 20, 23, 29);
+		List<Integer> list = new ArrayList<>();
+		
+		try {
+    		bst.insert(7, "7");
+    		bst.insert(14, "14");
+    		bst.insert(18, "18");
+    		bst.insert(23, "23");
+    		bst.insert(1, "1");
+    		bst.insert(11, "11");
+    		bst.insert(20, "20"); 
+    		bst.insert(29, "29");
+    		list = bst.getInOrderTraversal();
+    		assert(list.size() == expected.size());
+    		for (int i = 0; i < list.size(); i++) {
+    			assert(list.get(i) == expected.get(i));
+    		}
+		} catch (Exception e) {
+    		e.printStackTrace();
+    		fail( "Unexpected exception: "+e.getMessage());
+		}
+	}
+}
